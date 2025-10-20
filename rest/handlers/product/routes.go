ad1cd34a99899fec0d9d047cc30760fdae2f1976
rzhbadhon <rhbadhon030@gmail.com>
2025-10-20 23:04:34 +0600

    req route added

--- rest/handlers/product/routes.go
+++ rest/handlers/product/routes.go
@@ -6,4 +6,5 @@ import (
 
 func (h *ProductHandler) RegisterRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /api/v1/products/barcode/{barcode}", http.HandlerFunc(h.GetProduct))
+	mux.Handle("POST /api/v1/request", http.HandlerFunc(h.ReqProduct))
 }
