eeb7958b77c1644185e59b1da005d44d314caaaf
rzhbadhon <rhbadhon030@gmail.com>
2025-10-30 00:30:34 +0600

    completed

--- rest/handlers/user/update_user.go
+++ rest/handlers/user/update_user.go
@@ -7,7 +7,7 @@ import (
 	"ecoscan.com/repo"
 )
 
-func UpdateUserInfo(w http.ResponseWriter, r *http.Request) {
+func (h *UserHandler) UpdateUserInfo(w http.ResponseWriter, r *http.Request) {
 	uID := r.Context().Value("userID")
 
 	// decode the body 
@@ -21,10 +21,32 @@ func UpdateUserInfo(w http.ResponseWriter, r *http.Request) {
 
 	reqUpdateUser.ID = uID.(int64)
 
+	query := `
+    UPDATE users 
+    SET name = $1, email = $2, updated_at = NOW()
+    WHERE id = $3
+    RETURNING id, name, email, points, created_at, updated_at
+	`
+	row := h.DB.QueryRowContext(r.Context(), query, reqUpdateUser.Name,
+	reqUpdateUser.Email,
+	reqUpdateUser.ID,
+	)
 
+	var updatedUser repo.User
+	err = row.Scan(
+		&updatedUser.ID,
+		&updatedUser.Name,
+		&updatedUser.Email,
+        &updatedUser.Points,
+        &updatedUser.CreatedAt,
+        &updatedUser.UpdatedAt,
+	)
+	if err != nil{
+		http.Error(w, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
 
-	
-
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedUser)
 
 }
