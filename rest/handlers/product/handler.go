4cc690cb4463db7b40889d8d164345b73a22778a
rzhbadhon <rhbadhon030@gmail.com>
2025-10-31 23:43:20 +0600

    fix

--- rest/handlers/product/handler.go
+++ rest/handlers/product/handler.go
@@ -14,29 +14,30 @@ type ProductHandler struct {
 func NewProductHandler(db *sqlx.DB) *ProductHandler {
 	return &ProductHandler{
 		DB: db,
+		Store: NewMessageStore(),
 	}
 }
 
 type MessageStore struct {
-	mu       sync.RWMutex
-	messages map[string]string
+    mu   sync.RWMutex
+    data map[string]string
 }
 
 func NewMessageStore() *MessageStore {
-	return &MessageStore{
-		messages: make(map[string]string),
-	}
+    return &MessageStore{
+        data: make(map[string]string),
+    }
 }
 
 func (s *MessageStore) Set(barcode, msg string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.messages[barcode] = msg
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.data[barcode] = msg
 }
 
 func (s *MessageStore) Get(barcode string) (string, bool) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	msg, ok := s.messages[barcode]
-	return msg, ok
+    s.mu.RLock()
+    defer s.mu.RUnlock()
+    msg, ok := s.data[barcode]
+    return msg, ok
 }
